507bc7792fc260e96a12f712166e13f79b94593e
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-10-27 12:05:05 -0300

    banco de dados criado, container criado, visualizando no Beekeeper

--- src/routes.js
+++ src/routes.js
@@ -1,9 +1,10 @@
 const { Router } = require('express');
+import { Router } from 'express';
 
 const routes = new Router();
 
 routes.get('/', (request, response) => {
-    return response.status(200).json({ message: 'Hello World Bia!'});
+    return response.status(200).json({ message: 'Hello World Bia 10!'});
 });
 
-module.exports = routes;
+export default routes;
