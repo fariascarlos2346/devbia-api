c7dd3c2fc01e71dded02a3bf58a404c633819c5c
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-10-28 21:11:39 -0300

    persistindo os dados no banco

--- src/routes.js
+++ src/routes.js
@@ -1,10 +1,19 @@
-const { Router } = require('express');
 import { Router } from 'express';
+import { v4 } from 'uuid';
+
+import User from './app/models/User';
 
 const routes = new Router();
 
-routes.get('/', (request, response) => {
-    return response.status(200).json({ message: 'Hello World Bia 10!'});
+routes.get('/', async (request, response) => {
+    const user = await User.create({
+        id: v4(),
+        name: 'Bia2',
+        email: 'bia2@email.com',
+        password_hash: 'asdfgçlkjh',
+    });
+
+    return response.status(201).json(user);
 });
 
 export default routes;
