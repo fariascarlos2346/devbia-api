cc991670c7dfccd90c999a5c0464b1c8573d1ff8
fariascarlos2346 <fariascarlos2346@gmail.com>
2024-10-28 23:16:56 -0300

    validando e mostrando todos os erros

--- src/routes.js
+++ src/routes.js
@@ -1,19 +1,8 @@
 import { Router } from 'express';
-import { v4 } from 'uuid';
-
-import User from './app/models/User';
+import UserController from './app/controllers/UserController';
 
 const routes = new Router();
 
-routes.get('/', async (request, response) => {
-    const user = await User.create({
-        id: v4(),
-        name: 'Bia2',
-        email: 'bia2@email.com',
-        password_hash: 'asdfgçlkjh',
-    });
-
-    return response.status(201).json(user);
-});
+routes.post('/users', UserController.store);
 
 export default routes;
